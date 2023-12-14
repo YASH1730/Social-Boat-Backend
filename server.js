c22baf620a69aeb36f148e2d7b80df01a0767d04
YASH1730 <yashwantsahu3002@gmail.com>
2023-12-14 19:54:39 +0530

    fix : port switch

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 require('./database/db_config')
 
 app.use(cors())
@@ -15,7 +15,7 @@ app.use('/api',require('./routes/routes'))
 
 
 app.listen(PORT,()=>{
-    console.log("Server is running at 8080")
+    console.log('Server is running at ${PORT}')
 })
 
 
